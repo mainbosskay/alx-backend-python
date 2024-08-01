a6a6cfc6a4be36a582b33936f633d412edd69e35
mainbosskay <dadakehinde80@yahoo.com>
2024-08-01 18:20:38 +0000

    Python program that writes a type-annotated function add that takes a float a and a float b

---
+++ 0x00-python_variable_annotations/0-add.py
@@ -0,0 +1,7 @@
+#!/usr/bin/python3
+"""Module for type-annoted function has been estblished"""
+
+
+def add(a: float, b: float) -> float:
+    """Type-annotated function that adds two float numbers"""
+    return a + b
