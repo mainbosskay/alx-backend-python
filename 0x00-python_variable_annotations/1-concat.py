480d77aff4c26f5bc805ce5541728017b6734f45
mainbosskay <dadakehinde80@yahoo.com>
2024-08-01 18:33:29 +0000

    Python program that writes a type-annotated function concat that takes a string str1 and a string str2 as arguments and returns a concatenated string

---
+++ 0x00-python_variable_annotations/1-concat.py
@@ -0,0 +1,7 @@
+#!/usr/bin/env python3
+"""Module for type-annoted function concat is established"""
+
+
+def concat(str1: str, str2: str) -> str:
+    """Type-annotated function that concatenates two strings"""
+    return str1 + str2
