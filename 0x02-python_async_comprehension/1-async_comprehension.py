fc625c4e669b7c104e6cce16991b53a814a0c231
mainbosskay <dadakehinde80@yahoo.com>
2024-08-06 12:21:55 +0000

    Import async_generator then write a coroutine called async_comprehension that takes no arguments

---
+++ 0x02-python_async_comprehension/1-async_comprehension.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python3
+"""Module for async function async_comprehension is established"""
+from typing import List
+from importlib import import
+
+
+async_generator = using('0-async_generator').async_generator
+
+
+async def async_comprehension() -> List[float]:
+    """Asynchronously collects 10 random floats from an
+    async generator and returns them as list"""
+    return [num_random async for num_random in async_generator()]
